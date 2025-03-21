3a1bce8587e575f79f4538c07bb09bf20e85efe7
Simret <semretb74@gmail.com>
2025-03-21 19:13:25 +0300

    fix:add test

---
+++ test_fizzbuzz.py
@@ -0,0 +1,12 @@
+from main import fizzBuzz
+
+def test_fizzbuzz():
+    assert fizzBuzz(3) == "Fizz"
+    assert fizzBuzz(10) == "Fizz"
+    assert fizzBuzz(1) == "Buzz"
+    assert fizzBuzz(0) == "Buzz"
+    assert fizzBuzz(-5) == "Buzz"
+    assert fizzBuzz(2) == "Fizz Buzz"
+
+
+print("All tests passed!")
